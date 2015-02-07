78ebef60e411bf0e561ff978704afc87ec19e376
Alonzo Thomas <lenell16@gmail.com>
2015-02-07 17:35:01 -0500

    Test PreloadJS for Images

--- assets/js/newApp.js
+++ assets/js/newApp.js
@@ -1,24 +1,46 @@
+var canvas, stage;
+var images;
+var img;
+var loader;
+
 function init() {
-  var stage = new createjs.Stage("over-canvas");
-  var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    canvas = document.getElementById("over-canvas");
+    stage = new createjs.Stage(canvas);
+    images = images||{};
 
+    var manifest = [
+        {src:"./assets/img/car.png", id:"ship"}
+    ];
 
-  var dragger = new createjs.Container();
-  dragger.x = dragger.y = 100;
-  dragger.addChild(circle);
-  stage.addChild(dragger);
-
-  dragger.on("pressmove",function(evt) {
-    // currentTarget will be the container that the event listener was added to:
-    evt.currentTarget.x = evt.stageX;
-    evt.currentTarget.y = evt.stageY;
-    // make sure to redraw the stage to show the change:
-    stage.update();
-  });
-
-  stage.update();
+    loader = new createjs.LoadQueue();;
+    loader.onFileLoad = handleFileLoad;
+    loader.onComplete = handleComplete;
+    loader.loadManifest(manifest);
 }
+
+function handleFileLoad(o) {
+     //Alternatively you could store off the results as they get loaded.
+     //In this case we only have one image getting loaded
+    //if (o.type == "image") { images[o.id] = o.result; }
+}
+
+function handleComplete(event) {
+  /*getResult returns an object containing the contents of the object that was initially requested using loadFile or loadManifest, including:
+      src: The source of the file that was requested.
+      type: The type of file that was loaded. If it was not specified, this is auto-detected by PreloadJS using the file extension.
+      id: The id of the loaded object. If it was not specified, the ID will be the same as the "src" property.
+      data: Any arbitrary data that was specified, otherwise it will be undefined.
+  */
+    var path = loader.getResult("ship").src;
+    img = new createjs.Bitmap(path);
+    stage.addChild(img);
+    img.x = 100;
+    img.y = 100;
+
+    stage.update();
+}
+
+
 
 // Intialize Google Maps
 function initialize() {
